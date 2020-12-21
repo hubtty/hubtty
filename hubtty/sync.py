hubtty/hubtty

commit fb6c709f7d26b4cef0e7b579c7e3da1c029ae103

Make comments belong to change
Create a 1:n relationship between changes and comments to allows
inserting all comments that belong to a PR even when the commit from
which this comment originated no longer exists in github.

hubtty/sync.py
@@ -630,11 +630,11 @@ class SyncChangeTask(Task):
         remote_issue_comments = sync.get(('repos/%s/comments'
                                           % self.change_id).replace('/pulls/',
                                                                     '/issues/'))
+        # TODO(mandre)
+        # Save all comments to DB, then attach them to commits
 
         # Perform subqueries this task will need outside of the db session
         for remote_commit in remote_commits:
-            remote_commit['_hubtty_remote_comments_data'] = [comment for comment in remote_pr_comments
-                                                             if comment['original_commit_id'] == remote_commit['sha']]
             remote_commit['_hubtty_remote_reviews_data'] = [review for review in remote_pr_reviews
                                                             if review['commit_id'] == remote_commit['sha']]
             # remote_robot_comments_data = sync.get('changes/%s/revisions/%s/robotcomments' % (
@@ -755,39 +755,6 @@ class SyncChangeTask(Task):
                         f = commit.createFile(file['filename'], file['status'],
                                                 file.get('previous_filename'),
                                                 inserted, deleted)
-
-                remote_comments_items = remote_commit['_hubtty_remote_comments_data']
-                # remote_robot_comments_items = list(remote_commit.get('_hubtty_remote_robot_comments_data', {}).items())
-                # for remote_file, remote_comments in itertools.chain(remote_comments_items, remote_robot_comments_items):
-
-                for remote_comment in remote_comments_items:
-                    account = session.getAccountByID(remote_comment['user']['id'],
-                                                        username=remote_comment['user'].get('login'))
-                    comment = session.getCommentByID(remote_comment['id'])
-                    if not comment:
-                        # Normalize updated -> created
-                        created = dateutil.parser.parse(remote_comment['updated_at'])
-                        parent = False
-                        if remote_comment.get('side', '') == 'PARENT':
-                            parent = True
-                        fileobj = commit.getFile(remote_comment['path'])
-                        if fileobj is None:
-                            fileobj = commit.createFile(remote_comment['path'], 'M')
-                        comment = fileobj.createComment(remote_comment['id'], account,
-                                                        remote_comment.get('in_reply_to_id'),
-                                                        created,
-                                                        parent, remote_comment.get('line'),
-                                                        remote_comment.get('body','').replace('\r',''),
-                                                        robot_id = remote_comment.get('robot_id'),
-                                                        robot_run_id = remote_comment.get('robot_run_id'),
-                                                        url = remote_comment.get('html_url'))
-                        self.log.info("Created new comment %s for revision %s in local DB.",
-                                      comment.key, commit.key)
-                    else:
-                        if comment.author != account:
-                            comment.author = account
-                        comment.body = remote_comment.get('body','').replace('\r','')
-                        # TODO(mandre) Add `updated` column to comment table
 
                 # Commit checks
                 if remote_commit.get('_hubtty_remote_checks_data'):
@@ -952,6 +919,52 @@ class SyncChangeTask(Task):
 
             # remote_hashtags = remote_change.get('hashtags', [])
             # change.setHashtags(remote_hashtags)
+
+            for remote_comment in remote_pr_comments:
+                account = session.getAccountByID(remote_comment['user']['id'],
+                                                 username=remote_comment['user'].get('login'))
+                comment = session.getCommentByID(remote_comment['id'])
+
+                file_id = None
+                commit = session.getCommitBySha(remote_comment['commit_id'])
+                if commit:
+                    fileobj = commit.getFile(remote_comment['path'])
+                    if fileobj is None:
+                        fileobj = commit.createFile(remote_comment['path'], 'M')
+                    file_id = fileobj.key
+
+                if not comment:
+                    created = dateutil.parser.parse(remote_comment['created_at'])
+                    updated = dateutil.parser.parse(remote_comment['updated_at'])
+                    parent = False
+                    if remote_comment.get('side', '') == 'PARENT':
+                        parent = True
+
+                    comment = change.createComment(file_id, remote_comment['id'], account,
+                                                   remote_comment.get('in_reply_to_id'),
+                                                   created, updated, parent,
+                                                   remote_comment.get('commit_id'),
+                                                   remote_comment.get('original_commit_id'),
+                                                   remote_comment.get('line'),
+                                                   remote_comment.get('original_line'),
+                                                   remote_comment.get('body','').replace('\r',''),
+                                                   robot_id = remote_comment.get('robot_id'),
+                                                   robot_run_id = remote_comment.get('robot_run_id'),
+                                                   url = remote_comment.get('html_url'))
+                    self.log.info("Created new comment %s for change %s in local DB.",
+                                    comment.key, change.change_id)
+                else:
+                    if comment.author != account:
+                        comment.author = account
+                    if comment.updated != updated:
+                        comment.updated = updated
+                    if comment.commit_id != remote_comment.get('commit_id'):
+                        comment.commit_id = remote_comment.get('commit_id')
+                    if comment.line != remote_comment.get('line'):
+                        comment.line = remote_comment.get('line')
+                    if comment.file_id != file_id:
+                        comment.file_id = file_id
+                    comment.body = remote_comment.get('body','').replace('\r','')
 
             change.outdated = False
         for url, refs in fetches.items():

hubtty/alembic/versions/9ea887cbb620_update_comments.py
@@ -1,0 +1,27 @@
+"""Update comments
+
+Revision ID: 9ea887cbb620
+Revises: fc35305967d4
+Create Date: 2020-12-18 17:23:50.956084
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '9ea887cbb620'
+down_revision = 'fc35305967d4'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    # TODO(mandre) add a foreign key contraint for change_key
+    op.add_column('comment', sa.Column('change_key', sa.Integer(), nullable=False, index=True))
+    op.add_column('comment', sa.Column('updated', sa.DateTime(), nullable=False))
+    op.add_column('comment', sa.Column('commit_id', sa.String(length=255), nullable=False))
+    op.add_column('comment', sa.Column('original_commit_id', sa.String(length=255), nullable=False))
+    op.add_column('comment', sa.Column('original_line', sa.Integer(), nullable=False))
+
+
+def downgrade():
+    pass

hubtty/db.py
@@ -123,13 +123,18 @@ message_table = Table(
 comment_table = Table(
     'comment', metadata,
     Column('key', Integer, primary_key=True),
+    Column('change_key', Integer, ForeignKey("change.key"), index=True),
     Column('file_key', Integer, ForeignKey("file.key"), index=True),
     Column('account_key', Integer, ForeignKey("account.key"), index=True),
     Column('id', String(255), index=True), #, unique=True, nullable=False),
     Column('in_reply_to', String(255)),
     Column('created', DateTime, index=True, nullable=False),
+    Column('updated', DateTime, index=True, nullable=False),
     Column('parent', Boolean, nullable=False),
+    Column('commit_id', String(255), index=True, nullable=False),
+    Column('original_commit_id', String(255), index=True, nullable=False),
     Column('line', Integer),
+    Column('original_line', Integer),
     Column('message', Text, nullable=False),
     Column('draft', Boolean, index=True, nullable=False),
     Column('robot_id', String(255)),
@@ -428,6 +433,15 @@ class Change(object):
             if hashtag not in current_hashtags:
                 self.createHashtag(hashtag)
 
+    def createComment(self, *args, **kw):
+        session = Session.object_session(self)
+        args = [self] + list(args)
+        c = Comment(*args, **kw)
+        self.comments.append(c)
+        session.add(c)
+        session.flush()
+        return c
+
     @property
     def author_name(self):
         author_name = 'Anonymous Coward'
@@ -535,15 +549,22 @@ class Message(object):
         return author_name
 
 class Comment(object):
-    def __init__(self, file, id, author, in_reply_to, created, parent, line, message, draft=False,
-                 robot_id=None, robot_run_id=None, url=None):
-        self.file_key = file.key
+    def __init__(self, change, file_id, id, author, in_reply_to, created,
+                 updated, parent, commit_id, original_commit_id, line,
+                 original_line, message, draft=False, robot_id=None,
+                 robot_run_id=None, url=None):
+        self.change_key = change.key
+        self.file_key = file_id
         self.account_key = author.key
         self.id = id
         self.in_reply_to = in_reply_to
         self.created = created
+        self.updated = updated
         self.parent = parent
+        self.commit_id = commit_id
+        self.original_commit_id = original_commit_id
         self.line = line
+        self.original_line = original_line
         self.message = message
         self.draft = draft
         self.robot_id = robot_id
@@ -636,15 +657,6 @@ class File(object):
         else:
             return '%s => %s' % (self.old_path, self.path)
 
-    def createComment(self, *args, **kw):
-        session = Session.object_session(self)
-        args = [self] + list(args)
-        c = Comment(*args, **kw)
-        self.comments.append(c)
-        session.add(c)
-        session.flush()
-        return c
-
 class Server(object):
     def __init__(self):
         pass
@@ -703,6 +715,9 @@ mapper(Change, change_table, properties=dict(
                                cascade='all, delete-orphan'),
         commits=relationship(Commit, backref='change',
                              order_by=commit_table.c.number,
+                             cascade='all, delete-orphan'),
+        comments=relationship(Comment, backref='change',
+                             order_by=comment_table.c.created,
                              cascade='all, delete-orphan'),
         messages=relationship(Message,
                               secondary=commit_table,

hubtty/view/diff.py
@@ -528,12 +528,14 @@ class BaseDiffView(urwid.WidgetWrap, mywid.Searchable):
         if file_key is None:
             raise Exception("Comment is not associated with a file")
         with self.app.db.getSession() as session:
-            fileobj = session.getFile(file_key)
             account = session.getOwnAccount()
-            comment = fileobj.createComment(None, account, None,
+            change = session.getChange(self.change_key)
+            commit = session.getCommit(self.new_commit_key)
+            comment = change.createComment(file_key, None, account, None,
                                             datetime.datetime.utcnow(),
-                                            parent,
-                                            line_num, text, draft=True)
+                                            datetime.datetime.utcnow(),
+                                            parent, commit.sha, commit.sha,
+                                            line_num, line_num, text, draft=True)
             key = comment.key
         return key
 

commit 7c573cd1ae5f7836ce795e95bf425998a0347f7e

Fix sync after recent changes
This commit fixes the following errors introduced in
fb6c709f7d26b4cef0e7b579c7e3da1c029ae103:

UnboundLocalError: local variable 'updated' referenced before assignment

AttributeError: 'Comment' object has no attribute 'file_id'

hubtty/sync.py
@@ -888,9 +888,9 @@ class SyncChangeTask(Task):
                         fileobj = associated_commit.createFile(remote_comment['path'], 'M')
                     file_id = fileobj.key
 
+                updated = dateutil.parser.parse(remote_comment['updated_at'])
                 if not comment:
                     created = dateutil.parser.parse(remote_comment['created_at'])
-                    updated = dateutil.parser.parse(remote_comment['updated_at'])
                     parent = False
                     if remote_comment.get('side', '') == 'PARENT':
                         parent = True
@@ -917,8 +917,8 @@ class SyncChangeTask(Task):
                         comment.commit_id = remote_comment.get('commit_id')
                     if comment.line != remote_comment.get('line'):
                         comment.line = remote_comment.get('line')
-                    if comment.file_id != file_id:
-                        comment.file_id = file_id
+                    if comment.file_key != file_id:
+                        comment.file_key = file_id
                     comment.body = remote_comment.get('body','').replace('\r','')
 
             # Commit reviews