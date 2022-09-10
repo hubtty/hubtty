cb52e92844f4a2684445905e2188ce2791d74694
Martin André <m.andre@redhat.com>
2022-09-10 21:11:39 +0200

    Show label colors in label edit dialog

--- hubtty/view/pull_request.py
+++ hubtty/view/pull_request.py
@@ -58,7 +58,9 @@ class EditLabelsDialog(urwid.WidgetWrap, mywid.LineBoxTitlePropertyMixin):
 
         rows.append(urwid.Text(u"Labels:"))
         for label in pr.repository.labels:
+            palette_item = "label_" + str(label.id)
             b = mywid.FixedCheckBox(label.name, state=(label in pr.labels))
+            b.set_label((palette_item, label.name))
             rows.append(b)
             self.labels_checkboxes.append(b)
         rows.append(urwid.Divider())
